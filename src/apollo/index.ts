598cd652bca13c887b5083ec7815ef5e62e737b4
Julian Gomez Sibecas <juliangsibecas@gmail.com>
2022-09-02 15:35:06 -0300

    Add refresh token logic

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -7,7 +7,20 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+
 import { env } from '../env';
+import { SignInFromRefreshTokenMutationResult } from '../api';
+
+const REFRESH_TOKEN_QUERY = `
+  mutation {
+    signInFromRefreshToken {
+      userId
+      accessToken
+      refreshToken
+    }
+  }
+`;
 
 const httpLink = createHttpLink({
   uri: `${env.apiUrl}/graphql`,
@@ -18,6 +31,7 @@ const authLink = setContext(async (_, { headers }) => {
   const token = await SecureStore.getItemAsync('accessToken');
 
   return {
+    accessToken: token,
     headers: {
       ...headers,
       Authorization: token ? `Bearer ${token}` : '',
@@ -25,24 +39,61 @@ const authLink = setContext(async (_, { headers }) => {
   };
 });
 
-const tokenRefreshLink = new TokenRefreshLink({
+const tokenRefreshLink = new TokenRefreshLink<{
+  accessToken: string;
+  refreshToken: string;
+}>({
+  isTokenValidOrUndefined: async (operation) => {
+    try {
+      const token: string = operation.getContext().accessToken;
+      if (token) {
+        const decoded = jwtDecode(
+          operation.getContext().accessToken
+        ) as JwtPayload;
+
+        return Date.now() > decoded.exp!;
+      }
+
+      return false;
+    } catch (e) {
+      console.log(e);
+
+      return false;
+    }
+  },
   fetchAccessToken: async () => {
-    return fetch('http://localhost:4000/graphql', {
-      method: 'POST',
-      headers: {
-        Refresh: (await SecureStore.getItemAsync('refreshToken')) as string,
-      },
-    });
+    try {
+      const res = await fetch(`${env.apiUrl}/graphql`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Refresh: (await SecureStore.getItemAsync('refreshToken'))!,
+        },
+        body: JSON.stringify({
+          query: REFRESH_TOKEN_QUERY,
+        }),
+      });
+
+      return res.json();
+    } catch (e) {
+      console.log(e);
+      return new Response();
+    }
   },
-  handleFetch: () => {
-    return {
-      accessToken: '',
-    };
+  handleResponse: () => (response: SignInFromRefreshTokenMutationResult) => {
+    const accessToken = response.data?.signInFromRefreshToken;
+    return { access_token: accessToken };
   },
-  isTokenValidOrUndefined: () => false,
+  handleFetch: async ({ accessToken, refreshToken }) => {
+    await SecureStore.setItemAsync('accessToken', accessToken);
+    await SecureStore.setItemAsync('refreshToken', refreshToken);
+  },
+  handleError: (err) => {
+    console.log(err);
+  },
 });
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: ApolloLink.from([authLink, httpLink]),
+  link: ApolloLink.from([authLink, tokenRefreshLink, httpLink]),
 });
