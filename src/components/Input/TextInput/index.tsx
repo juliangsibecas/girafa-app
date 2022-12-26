82cfc2c0831f9391a3e3edd7c243559d5b6f42ae
Julian Gomez Sibecas <juliangsibecas@gmail.com>
2022-12-26 12:21:39 -0300

    Add text input height fix

--- src/components/Input/TextInput/index.tsx
+++ src/components/Input/TextInput/index.tsx
@@ -32,7 +32,7 @@ export const TextInput: React.FC<ITextInput> = ({
   const { theme } = useTheme();
 
   const style = useStyle({
-    py: 1.3,
+    py: 2,
     px: 2,
     bgColor: 'disabled',
     color: 'text.primary',
